4f14ea74f73a0b1e1018d236b7f12146aa9e016c
Corentin Schreiber <cschreib@orange.fr>
2018-09-06 15:29:54 +0100

    Updated to latest API

--- psf-averager-egg.cpp
+++ psf-averager-egg.cpp
@@ -20,6 +20,13 @@ public :
 };
 
 int phypp_main(int argc, char* argv[]) {
+    // External data
+    std::string share_dir = "/home/cschreib/code/egg-analytic/share/";
+    std::string filter_db = "/home/cschreib/code/euclid_psf/psf-averager/filters.dat";
+    std::string sed_lib = "/home/cschreib/code/egg-analytic/share/opt_lib_fastpp_hd_noigm.fits";
+    std::string sed_imf = "chabrier";
+    std::string psf_file  = "/home/cschreib/code/euclid_psf/psf-averager/psf-mono.fits";
+
     // Survey definition
     double maglim = 24.5;
     std::string selection_band = "euclid-vis";
@@ -29,9 +36,11 @@ int phypp_main(int argc, char* argv[]) {
     uint_t nthread = 0;
     bool write_individuals = false;
     bool write_averages = true;
+    std::string cache_id;
 
     read_args(argc, argv, arg_list(maglim, selection_band, dz, seds_step, iz,
-        nthread, write_individuals, write_averages));
+        nthread, write_individuals, write_averages, cache_id, share_dir, filter_db,
+        sed_lib, sed_imf, psf_file));
 
     egg_averager pavg;
     pavg.write_cache = false;
@@ -39,8 +48,10 @@ int phypp_main(int argc, char* argv[]) {
 
     // Setup survey
     egg::generator_options opts;
-    opts.share_dir = "/home/cschreib/code/egg/share/";
-    opts.filter_db = "/home/cschreib/code/euclid_psf/psf-averager/filters.dat";
+    opts.share_dir = share_dir;
+    opts.filter_db = filter_db;
+    opts.sed_lib = sed_lib;
+    opts.sed_lib_imf = sed_imf;
     opts.filter_flambda = true;
     opts.filter_photons = true;
     opts.selection_band = selection_band;
@@ -58,9 +69,10 @@ int phypp_main(int argc, char* argv[]) {
     mopts.min_mag_err = 0.0;
     mopts.dz = dz;
     mopts.no_noise = true;
-    mopts.psf_file = "/home/cschreib/code/euclid_psf/psf-averager/psf-mono.fits";
+    mopts.psf_file = psf_file;
     mopts.write_individuals = write_individuals;
     mopts.write_averages = write_averages;
+    mopts.force_cache_id = cache_id;
     pavg.configure_mock(mopts);
 
     if (!pavg.average_redshift_bin(iz)) return 1;
